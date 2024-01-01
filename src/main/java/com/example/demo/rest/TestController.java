aba8c064c5bb387f18cd071c63f0fe405acba2db
Ish Mahajan <ishmahajanbhanu@gmail.com>
2024-01-01 16:19:02 +0530

    remove unwanted dependency

--- src/main/java/com/example/demo/rest/TestController.java
+++ src/main/java/com/example/demo/rest/TestController.java
@@ -3,15 +3,14 @@ package com.example.demo.rest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @Slf4j
 @RestController
 public class TestController {
 
     @GetMapping("hello")
-    public Mono<String> hello(){
-        log.info("Say Hellowww loudly");
-        return Mono.fromSupplier(()-> "Say Hello");
+    public String hello(){
+        log.info("Say Hello loudly along with trace Id");
+        return "Hi! Hello";
     }
 }
